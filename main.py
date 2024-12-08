355878ab11e9e0e155a88eaeaf95d587328c8114
threatdavid <itsdavid.dev@gmail.com>
2024-12-08 00:27:42 -0500

    Added read file function

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+def read_file(file_path: str) -> str:
+    with open(file_path) as file:
+        return file.read()
+
+
 def main():
     path = "book/frankenstein.txt"
-    
+    content = read_file(path)
 
 if __name__ == "__main__":
     main()
