cd2ded0fccb52b7fc175bc19cd5ab28dee1fa58b
threatdavid <itsdavid.dev@gmail.com>
2024-12-08 00:31:40 -0500

    Added word count function

--- main.py
+++ main.py
@@ -3,9 +3,14 @@ def read_file(file_path: str) -> str:
         return file.read()
 
 
+def word_count(text: str) -> int:
+    return len(text.split())
+
+
 def main():
     path = "book/frankenstein.txt"
     content = read_file(path)
+    word_count_result = word_count(content)
 
 if __name__ == "__main__":
     main()
