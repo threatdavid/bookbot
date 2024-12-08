407f5e162ed5c68d334ef0caafd00a4ad0a27b0e
threatdavid <itsdavid.dev@gmail.com>
2024-12-08 11:46:59 -0500

    Added log report feature

--- main.py
+++ main.py
@@ -14,12 +14,25 @@ def get_char_f(text: str) -> dict[str, int]:
     return f_table
 
 
+def sort_f(char_f: dict[str, int]): 
+    return dict(sorted(char_f.items(), key=lambda char: char[1], reverse=True))
+
+
+def log_report(book_path: str, word_count: int, char_f: dict[str, int]) -> str:
+    print(f"--- Begin report of {book_path} ---")
+    print(f"{word_count} words found in the document\n")    
+    for char in sort_f(char_f):
+        print(f"The '{char}' character was found {char_f[char]} times")
+    print("--- End report ---")
+
+
 def main():
     path = "book/frankenstein.txt"
     content = read_file(path)
     word_count_result = word_count(content)
     char_f = get_char_f(content)
-    
+    log_report(path, word_count_result, char_f)
+
 
 if __name__ == "__main__":
     main()
